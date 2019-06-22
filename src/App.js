c3cf7e34b7dbe4c9a1d8bcf10b81b160ace518d3
Esther Grigati <esther.bsb@gmail.com>
2019-06-22 16:26:30 -0300

    Adiciona modal para o formulário de cadastro

--- src/App.js
+++ src/App.js
@@ -1,17 +1,55 @@
 import React from 'react';
 import FormularioCadastroConta from './FormularioCadastroConta';
-import { Form } from 'antd';
+import { Form, Button, Modal } from 'antd';
 import ListaContasAPagar from './ListaContasAPagar';
 
-function App() {
-  const Formulario = Form.create({})(FormularioCadastroConta);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <ListaContasAPagar />
-      <Formulario />
-    </div>
-  );
+    this.state = {
+      mostrarModal: false
+    }
+  }
+
+  abrirModal = () => {
+    this.setState({
+      mostrarModal: true,
+    });
+  };
+
+  fecharModal = () => {
+    this.setState({
+      mostrarModal: false,
+    });
+  };
+
+  render () {
+    const CadastroConta = Form.create({})(FormularioCadastroConta);
+
+    return (
+      <div>
+        {/* Botão para abrir modal de cadastrar compras */}
+        <div style={{ display: "flex" }}>
+          <Button type="primary" style={{ margin: "20px auto" }} onClick={this.abrirModal}>
+            Cadastrar Conta a Pagar
+          </Button>
+        </div>
+
+        <ListaContasAPagar />
+
+        <Modal
+          title="Cadastrar Conta a Pagar"
+          style={{ top: "1em" }}
+          visible={this.state.mostrarModal}
+          onCancel={this.fecharModal}
+          footer={null}
+        >
+          <CadastroConta />
+        </Modal>
+      </div>
+    );
+  }
 }
 
 export default App;
