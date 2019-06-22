2a0c928590f58ab8a19cf50d93358335c02c5905
Esther Grigati <esther.bsb@gmail.com>
2019-06-22 16:12:51 -0300

    Adiciona componente para renderizar lista de contas a pagar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import FormularioCadastroConta from './FormularioCadastroConta';
 import { Form } from 'antd';
+import ListaContasAPagar from './ListaContasAPagar';
 
 function App() {
   const Formulario = Form.create({})(FormularioCadastroConta);
 
   return (
     <div>
+      <ListaContasAPagar />
       <Formulario />
     </div>
   );
---
+++ src/ListaContasAPagar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Table } from 'antd';
+import { buscarContasAPagar } from './backend';
+
+const { Column } = Table;
+
+class ListaContasAPagar extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            contas: null
+        }
+    }
+
+    componentDidMount() {
+        // Busca lista de contas a pagar do backend
+        const contas = buscarContasAPagar();
+        this.setState({ contas: contas });
+    }
+
+    render() {
+        if (!this.state.contas)
+            return null;
+
+        return (
+            <Table dataSource={this.state.contas} rowKey={conta => conta.id}>
+                <Column title="Descrição" dataIndex="descricao" key="descricao" />
+                <Column title="Quem Pagou a Conta" dataIndex="responsavel" key="responsavel" />
+                <Column 
+                    title="Valor" 
+                    dataIndex="valor" 
+                    key="valor" 
+                    render={valor => (
+                        <span>
+                            {valor.toLocaleString("pt-BR", { 
+                                minimumFractionDigits: 2, 
+                                style: 'currency', 
+                                currency: 'BRL'
+                            })}
+                        </span>
+                    )}
+                />
+                <Column 
+                    title="Data de Vencimento" 
+                    dataIndex="dataVencimento" 
+                    key="dataVencimento"
+                    render={dataVencimento => (
+                        <span>
+                            {new Date(dataVencimento).toLocaleDateString() }
+                        </span>
+                    )} />
+                <Column 
+                    title="Ciclo de Repetição" 
+                    dataIndex="cicloRepeticao" 
+                    key="cicloRepeticao"
+                    render={ciclo => (
+                        <span>
+                            {(ciclo === 1) ? 
+                                "Mensal" 
+                                : (ciclo === 6) ? "Semestral" : "Anual" 
+                            }
+                        </span>
+                    )} /> />
+                <Column title="Número de Repetições" dataIndex="numRepeticoes" key="numRepeticoes" />
+            </Table>
+        );
+    } 
+}
+
+export default ListaContasAPagar;
