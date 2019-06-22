66fe914d2fc40ae6c9aaa0625897312d9722b1d9
Esther Grigati <esther.bsb@gmail.com>
2019-06-22 15:35:32 -0300

    Ilustra o registro das informações

--- src/backend.js
+++ src/backend.js
@@ -11,3 +11,9 @@
 export function cadastrarContaAPagar(conta) {
     console.log('Valores recebidos: ', conta);
 }
+
+/* Chama o backend para marcar a conta como paga.
+ * A data de vencimento deve ser atualizada de acordo com o ciclo
+ * e o número de repetições deve ser decrementado em um
+ */
+export function pagarConta() { }
