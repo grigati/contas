0bd7c287d7c15e9edabd1626f6a06bd77268bbd0
Esther Grigati <esther.bsb@gmail.com>
2019-06-26 15:42:15 -0300

    Adiciona função para organizar as contas para a visualização

--- src/backend.js
+++ src/backend.js
@@ -47,6 +47,49 @@ export function buscarContasAPagar() {
             numRepeticoes: 12,
             responsavel: "Emily Santos",
             valor: 250
+        },
+        {
+            id: 4,
+            cicloRepeticao: 12,
+            dataVencimento: "2020-07-01",
+            descricao: "IPTU",
+            numRepeticoes: 3,
+            responsavel: "Antônia Gomes",
+            valor: 100
         }
     ];
+}
+
+// Organiza e ordena as contas para a visualização
+export function ordenarContasAPagar() {
+    const contas = buscarContasAPagar();
+    let retorno = [];
+
+    for (let i in contas) {
+        const conta = contas[i];
+        const { cicloRepeticao, dataVencimento, numRepeticoes } = conta;
+
+        // Adiciona conta com a data de vencimento atualizada à lista que será retornada
+        for (let repeticao=0; repeticao<numRepeticoes; repeticao++) {
+            // Atualiza a data de acordo com o ciclo de repetição
+            let data = new Date(dataVencimento);
+            data.setMonth(data.getMonth() + cicloRepeticao*repeticao);
+
+            // Passa para o formato yyyy-MM-dd
+            let novoVencimento = data.toISOString().slice(0, 10);
+
+            const novaConta = {
+                ...conta,
+                id: `conta-${conta.id}-${novoVencimento}`,
+                dataVencimento: novoVencimento
+            }
+
+            retorno.push(novaConta);
+        }
+    }
+
+    // Ordena as contas por data de vencimento
+    retorno.sort((a, b) => ((a.dataVencimento > b.dataVencimento) ? 1 : -1));
+
+    return retorno;
 }
